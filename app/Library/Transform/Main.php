2d8311c55ccce2b3d1d81b4c471bb2304cce16a6
tttlkkkl <yehong0000@163.com>
2016-12-15 00:34:00 +0800

    xml转换测试

--- app/Library/Transform/Main.php
+++ app/Library/Transform/Main.php
@@ -17,13 +17,16 @@ class Main {
     private $Head;
     //主体
     private $Body;
+    //是否将样式插入节点
+    private $insertCssIntoNode;
     //标签映射
     private static $labelMapping=[
-        ''
+        'para'=>'div',
+        'text'=>'div'
     ];
 
     private function __construct() {
-
+        $this->insertCssIntoNode=true;
     }
 
     /**
@@ -43,19 +46,62 @@ class Main {
      * @return string
      */
     public function transform($content) {
-        echo $content;
-        $Xml = simplexml_load_string($content);
+        //$Xml = simplexml_load_string($content);
+        $Xml=simplexml_load_file('/home/m/web/note/note.xml');
         if ($Xml === false) {
             throw new \Exception(libxml_get_last_error(), 4000);
         }
-        $this->head($Xml->note->head);
+        $this->execute($Xml);
     }
 
-    private function execute($Xml) {
+    private function execute(\SimpleXMLElement $Xml) {
+        $this->HtmlXml=new \SimpleXMLElement("<div id='article-warp'></div>");
+        $Xml=new \SimpleXMLIterator($Xml->asXML());
 
+        foreach($Xml->children() as $child){
+            if($child->getName() == 'head'){
+                $this->head($child);
+            }
+            if($child->getName()=='body'){
+                $this->body($child);
+            }
+        }
     }
 
-    private function head($Xml) {
-
+    /**
+     * @param \SimpleXMLElement $Xml
+     */
+    private function head(\SimpleXMLIterator $Xml) {
+        //echo $Xml->getName();
     }
+
+    /**
+     * @param \SimpleXMLElement $Xml
+     */
+    private function body(\SimpleXMLIterator $Xml) {
+        $H=new \SimpleXMLElement("<div id='article-warp'></div>");
+        foreach($Xml->children() as $key=>$child){
+            if(array_key_exists($key,self::$labelMapping)){
+                $H->addChild(self::$labelMapping[$key],' ');
+                var_dump(property_exists($child,'coId'));
+                echo $child->cocId;
+                //var_dump($child);
+                if(property_exists($child,'coId')){
+                    $H->children()->addAttribute('id',$child->coId);
+                }
+                if(property_exists($child,'text')){
+                    if($child->text){
+                        $H->children()->addChild('div',iconv('gbk','utf-8','xx'));
+                    }else{
+                        $H->children()->addChild('br');
+                    }
+                }
+            }
+            echo $H->asXML();
+            break;
+        }
+    }
+
 }
+
+Main::getInstance()->transform(null);
